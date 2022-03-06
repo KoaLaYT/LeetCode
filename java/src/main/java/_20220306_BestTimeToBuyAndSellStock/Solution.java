0fc71a0ac164907c10d69ebe33dcb8a6da41c9aa
KoaLaYT <hytohyeah@outlook.com>
2022-03-06 15:00:02 +0800

    best time to buy and sell stock

---
+++ java/src/main/java/_20220306_BestTimeToBuyAndSellStock/Solution.java
@@ -0,0 +1,18 @@
+package _20220306_BestTimeToBuyAndSellStock;
+
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int buyPrice = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - buyPrice);
+            buyPrice = Math.min(buyPrice, prices[i]);
+        }
+        return profit;
+    }
+
+}
