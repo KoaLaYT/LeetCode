242ca90d673612c2a20057991691bf24ce4832c5
KoaLaYT <hytohyeah@outlook.com>
2022-07-30 21:39:27 +0800

    substr

--- java/src/main/java/_20220729_Substr/Solution.java
+++ java/src/main/java/_20220729_Substr/Solution.java
@@ -2,48 +2,48 @@ package _20220729_Substr;
 
 public class Solution {
 
-    private static final long MODULO = 1000000007;
-
     public boolean substr(String a, String b) {
-        if (a.length() < b.length()) {
-            return false;
-        }
-
         char[] aa = a.toCharArray();
         char[] bb = b.toCharArray();
+        int[] states = buildState(bb);
 
-        long aNum = aa[0] - 'a';
-        long bNum = bb[0] - 'a';
-        long base = 1;
-        int i = 1;
-        for (; i < bb.length; i++) {
-            aNum = (aNum * 26 + (aa[i] - 'a')) % MODULO;
-            bNum = (bNum * 26 + (bb[i] - 'a')) % MODULO;
-            base = (base * 26) % MODULO;
+        return substr(aa, bb, states, bb.length - 1);
+    }
+
+    private int[] buildState(char[] bb) {
+        int[] states = new int[bb.length];
+        states[0] = -1;
+        int state = -1;
+
+        for (int i = 1; i < bb.length; i++) {
+            while (state >= 0 && bb[i] != bb[state + 1]) {
+                state = states[state];
+            }
+            if (bb[i] == bb[state + 1]) {
+                state += 1;
+            }
+            states[i] = state;
         }
 
-        while (true) {
-            if (aNum == bNum) {
-                return ensureEqual(aa, bb, i);
+        return states;
+    }
+
+    private boolean substr(char[] aa, char[] bb, int[] states, int finalState) {
+        int state = -1;
+
+        for (char c : aa) {
+            while (state >= 0 && c != bb[state + 1]) {
+                state = states[state];
             }
-            if (i >= aa.length) {
-                break;
+            if (c == bb[state + 1]) {
+                state += 1;
             }
-            aNum -= (aa[i - bb.length] - 'a') * base;
-            aNum = Math.floorMod(aNum * 26 + (aa[i] - 'a'), MODULO);
-            i++;
+            if (state == finalState) {
+                return true;
+            }
         }
 
         return false;
-    }
-
-    private static boolean ensureEqual(char[] aa, char[] bb, int k) {
-        for (int i = 0, j = k - bb.length; i < bb.length; i++, j++) {
-            if (aa[j] != bb[i]) {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
